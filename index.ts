7cf0b15598792e121ab9281d3af95282fe289c76
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 21:24:38 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q67
+// this function add a num and string that represent a num function
+function add(num1: number, numberstring: string) {
+    return (num1 + +numberstring)
+}
+console.log(add(10, "20"));
